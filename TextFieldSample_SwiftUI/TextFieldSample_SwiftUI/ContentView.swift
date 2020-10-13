09795f96c50fe5c9c08e12ad0c60a7b28dba7538
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-13 20:46:55 +0900

    初回コミット

---
+++ TextFieldSample_SwiftUI/TextFieldSample_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextFieldSample_SwiftUI
+//
+//  Created by home on 2020/10/13.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
